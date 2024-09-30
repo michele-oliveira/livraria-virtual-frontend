1b9660eec1819219adcbc49946f9fa7da7c84064
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-09-30 20:02:24 -0300

    Implementando busca com debounce

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,49 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getBooks } from "../api/books/books.api";
 
 import logo from "../assets/images/png-transparent-drawing-graphy-tree-of-life-others-removebg-preview.png";
 
 const Header = () => {
   const [search, setSearch] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
   const navigate = useNavigate();
+
+  const searchBooks = async (search) => {
+    try {
+      if (search) {
+        const books = await getBooks(search);
+        setSuggestions(books);
+      } else {
+        setSuggestions([]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   const handleSearch = () => {
     navigate(`/?search=${search}`);
   };
+
+  useEffect(() => {
+    if (!search) {
+      setSuggestions([]);
+    } else {
+      const handler = setTimeout(() => {
+        searchBooks(search);
+      }, 1000);
+
+      return () => {
+        clearTimeout(handler);
+      };
+    }
+  }, [search]);
 
   return (
     <>
@@ -32,19 +65,38 @@ const Header = () => {
               </h1>
             </Link>
           </div>
-          <div className="flex items-center w-full md:w-1/2 lg:w-1/3 border p-1 rounded-md">
-            <input
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              className="flex-grow pl-2 text-slate-500 text-sm h-full outline-none"
-              placeholder="Buscar"
-            />
-            <button type="button" onClick={handleSearch}>
-              <ion-icon
-                name="search-outline"
-                className="text-slate-500 mx-2 cursor-pointer"
-              ></ion-icon>
-            </button>
+          <div className="relative flex items-center flex-col h-8 w-full md:w-1/2 lg:w-1/3">
+            <div className="flex items-center w-full h-9 border p-1 rounded-md">
+              <input
+                value={search}
+                onChange={handleChangeSearch}
+                className="flex-grow pl-2 text-slate-500 text-sm h-full outline-none"
+                placeholder="Buscar"
+              />
+              <button type="button" onClick={handleSearch}>
+                <ion-icon
+                  name="search-outline"
+                  className="text-slate-500 mx-2 cursor-pointer"
+                ></ion-icon>
+              </button>
+            </div>
+            {suggestions.length > 0 && (
+              <div className="flex items-center w-full border mt-2 p-1 rounded-md bg-white border-gray-300 shadow-lg z-50">
+                {suggestions.map((suggestion) => (
+                  <div className="flex flex-row" key={suggestion.id}>
+                    <img
+                      src={suggestion.image_1}
+                      alt={`Contem a capa do livro ${suggestion.book_name}`}
+                      className="h-12"
+                    />
+                    <div className="ml-2">
+                      <h5>{suggestion.book_name}</h5>
+                      <p>{suggestion.author}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
           <div className="flex gap-4 mt-4 md:mt-0 ">
             <span className="text-slate-700 bg-slate-200 rounded-md p-2 h-8 hover:bg-slate-400">
