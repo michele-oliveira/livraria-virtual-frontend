efae80296b57acc75736091f28d09544cb9990dd
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-11-14 17:46:22 -0300

    Ajustando layout responsivo da página Book para telas muito grandes, limitando e centralizando as informações do livro

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -29,77 +29,79 @@ const BookItem = ({
   canDelete,
   handleClickDeleteButton
 }) => (
-  <section className="flex flex-col mt-4 md:flex-row p-6 rounded-lg">
-    <div className="flex-shrink-0 relative p-5">
-      <div className="flex justify-center">
-        <img
-          className="w-56 sm:w-64 md:w-72 rounded-lg shadow-md transition-opacity duration-300 ease-in-out group-hover:opacity-80"
-          src={book.image_1}
-          alt={`Contains the book cover of ${book.book_name}`}
-        />
-      </div>
+  <div className="flex justify-center">
+    <section className="flex flex-col mt-4 md:flex-row p-6 rounded-lg max-w-[96rem]">
+      <div className="flex-shrink-0 relative p-5">
+        <div className="flex justify-center">
+          <img
+            className="w-56 sm:w-64 md:w-72 rounded-lg shadow-md transition-opacity duration-300 ease-in-out group-hover:opacity-80"
+            src={book.image_1}
+            alt={`Contains the book cover of ${book.book_name}`}
+          />
+        </div>
 
-      <div className="w-full flex justify-center gap-2 p-5">
-        <a
-          href={book.book_file}
-          download={getFileNameFromDownloadUrl(book.book_file) || "book.pdf"}
-          target="_blank"
-          rel="noreferrer"
-          className="bg-slate-200 text-black px-2 sm:px-6 py-2 rounded-md hover:bg-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 text-sm flex items-center justify-center w-32 h-12"
-        >
-          Baixar
-        </a>
-        {(canAddFavorite && handleClickHeartButton) && (
-          <button
-            type="button"
-            onClick={handleClickHeartButton}
-            className="flex items-center justify-center w-12 h-12"
+        <div className="w-full flex justify-center gap-2 p-5">
+          <a
+            href={book.book_file}
+            download={getFileNameFromDownloadUrl(book.book_file) || "book.pdf"}
+            target="_blank"
+            rel="noreferrer"
+            className="bg-slate-200 text-black px-2 sm:px-6 py-2 rounded-md hover:bg-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 text-sm flex items-center justify-center w-32 h-12"
           >
-            <span className="bg-slate-200 rounded-md p-3 hover:bg-slate-400 text-xl flex justify-center">
-              <ion-icon name={isFavorite ? "heart" : "heart-outline"}></ion-icon>
-            </span>
-          </button>
-        )}
-        {(canEdit && handleClickEditButton) && (
-          <button
-            type="button"
-            onClick={handleClickEditButton}
-            className="flex items-center justify-center w-12 h-12"
-          >
-            <span className="bg-slate-200 rounded-md p-3 hover:bg-slate-400 text-xl flex justify-center">
-              <ion-icon name="pencil-outline"></ion-icon>
-            </span>
-          </button>
-        )}
-        {(canDelete && handleClickDeleteButton) && (
-          <button
-            type="button"
-            onClick={handleClickDeleteButton}
-            className="flex items-center justify-center w-12 h-12"
-          >
-            <span className="text-red-600 bg-slate-200 rounded-md p-3 hover:bg-slate-400 text-xl flex justify-center">
-              <ion-icon name="trash-outline"></ion-icon>
-            </span>
-          </button>
-        )}
+            Baixar
+          </a>
+          {(canAddFavorite && handleClickHeartButton) && (
+            <button
+              type="button"
+              onClick={handleClickHeartButton}
+              className="flex items-center justify-center w-12 h-12"
+            >
+              <span className="bg-slate-200 rounded-md p-3 hover:bg-slate-400 text-xl flex justify-center">
+                <ion-icon name={isFavorite ? "heart" : "heart-outline"}></ion-icon>
+              </span>
+            </button>
+          )}
+          {(canEdit && handleClickEditButton) && (
+            <button
+              type="button"
+              onClick={handleClickEditButton}
+              className="flex items-center justify-center w-12 h-12"
+            >
+              <span className="bg-slate-200 rounded-md p-3 hover:bg-slate-400 text-xl flex justify-center">
+                <ion-icon name="pencil-outline"></ion-icon>
+              </span>
+            </button>
+          )}
+          {(canDelete && handleClickDeleteButton) && (
+            <button
+              type="button"
+              onClick={handleClickDeleteButton}
+              className="flex items-center justify-center w-12 h-12"
+            >
+              <span className="text-red-600 bg-slate-200 rounded-md p-3 hover:bg-slate-400 text-xl flex justify-center">
+                <ion-icon name="trash-outline"></ion-icon>
+              </span>
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-    <div className="flex flex-col justify-center flex-1 p-10 mt-5 md:ml-4 bg-white border rounded-lg">
-      <h1 className="text-2xl font-bold text-gray-800 pb-5">
-        {book.book_name.toUpperCase()}
-      </h1>
-      <p className="text-gray-500">Editora: {book.publisher}</p>
-      <p className="text-gray-500">Idioma: {book.language}</p>
-      <p className="text-gray-500">Páginas: {book.pages}</p>
-      <p className="text-gray-500">Gênero: {book.subgender.gender.name}</p>
-      <p className="text-gray-500">Subgênero: {book.subgender.name}</p>
-      {book.description.split("\n").map((p, index) => (
-        <p className="text-gray-700 leading-relaxed text-sm mt-4" key={index}>
-          {p}
-        </p>
-      ))}
-    </div>
-  </section>
+      <div className="flex flex-col justify-center flex-1 p-10 mt-5 md:ml-4 bg-white border rounded-lg">
+        <h1 className="text-2xl font-bold text-gray-800 pb-5">
+          {book.book_name.toUpperCase()}
+        </h1>
+        <p className="text-gray-500">Editora: {book.publisher}</p>
+        <p className="text-gray-500">Idioma: {book.language}</p>
+        <p className="text-gray-500">Páginas: {book.pages}</p>
+        <p className="text-gray-500">Gênero: {book.subgender.gender.name}</p>
+        <p className="text-gray-500">Subgênero: {book.subgender.name}</p>
+        {book.description.split("\n").map((p, index) => (
+          <p className="text-gray-700 leading-relaxed text-sm mt-4" key={index}>
+            {p}
+          </p>
+        ))}
+      </div>
+    </section>
+  </div>
 );
 
 BookItem.propTypes = {
