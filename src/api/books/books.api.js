d69dd4f90794659006e2dcc44b0c32fea7fe985e
michele-oliveira <micheleoliveiranovais24@gmail.com>
2024-11-14 15:37:41 -0300

    Adicionando header Authorization nas requisições autenticadas e tratando erros de 401 Unauthorized

--- src/api/books/books.api.js
+++ src/api/books/books.api.js
@@ -1,3 +1,6 @@
+import { getJwt } from "../../utils/jwt";
+import UnauthorizedError from "../../errors/http/UnauthorizedError";
+
 export const getBookGenders = async () => {
   try {
     const response = await fetch(
@@ -60,10 +63,23 @@ export const getBook = async (bookId) => {
 
 export const newBook = async (newBookData) => {
   try {
+    const accessToken = getJwt();
+
     const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/books`, {
       method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
       body: newBookData,
     });
+    if (!response.ok && response.status === 401) {
+      throw new UnauthorizedError(
+        "Unauthorized attempt to create a book"
+      );
+    }
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     const newBook = await response.json();
     return newBook;
   } catch (error) {
@@ -74,10 +90,23 @@ export const newBook = async (newBookData) => {
 
 export const updateBook = async (updatedBookData) => {
   try {
+    const accessToken = getJwt();
+
     const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/books`, {
       method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
       body: updatedBookData,
     });
+    if (!response.ok && response.status === 401) {
+      throw new UnauthorizedError(
+        "Unauthorized attempt to edit book"
+      );
+    }
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     const newBook = await response.json();
     return newBook;
   } catch (error) {
@@ -88,12 +117,22 @@ export const updateBook = async (updatedBookData) => {
 
 export const deleteBook = async (bookId) => {
   try {
+    const accessToken = getJwt();
+
     const response = await fetch(
       `${process.env.REACT_APP_BACKEND_URL}/books/${bookId}`,
       {
         method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       }
     );
+    if (!response.ok && response.status === 401) {
+      throw new UnauthorizedError(
+        "Unauthorized attempt to delete book"
+      );
+    }
     if (!response.ok) {
       throw new Error(response.statusText);
     }
